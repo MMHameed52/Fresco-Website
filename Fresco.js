edf0ed9871815d2a2b650593452d02cd641ff648
Mustafaa Hameed <mohammadmustafaahameed@gmail.com>
2024-02-07 17:20:37 -0500

    fixed slideshow and created location card

--- Fresco.js
+++ Fresco.js
@@ -4,22 +4,22 @@ var currentImg = 0; // index of the first image
 const interval = 3000; // duration(speed) of the slide
 
 function changeSlide(n) {
-    for (var i = 0; i < imgs.length; i++) { // reset
-      imgs[i].style.opacity = 0;
-      dots[i].className = dots[i].className.replace(' active', '');
+    // Hide all images and deactivate all dots
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].style.opacity = 0;
+        dots[i].classList.remove('active');
     }
-  
-    var timer = setInterval(changeSlide, interval);
 
-currentImg = (currentImg + 1) % imgs.length; // update the index number
+    // Update currentImg index
+    currentImg = (n + imgs.length) % imgs.length;
 
-if (n != undefined) {
-    clearInterval(timer);
-    timer = setInterval(changeSlide, interval);
-    currentImg = n;
+    // Show the current image and activate the corresponding dot
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].classList.add('active');
 }
-  }
 
-
-
+// Automatically change slide at specified interval
+setInterval(function() {
+    changeSlide(currentImg + 1);
+}, interval);
 
